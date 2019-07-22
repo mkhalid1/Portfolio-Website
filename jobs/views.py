29ef33225342adae6294e78bacd631a26954a078
Muhammad Junaid <mkhalid.bese16seecs@seecs.edu.pk>
2019-07-23 01:16:02 +0500

    Jobs page completed

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Job
 
-# Create your views here.
+def home(Request):
+
+	jobs = Job.objects
+	return render(Request, 'jobs/home.html', {"jobs":jobs})
