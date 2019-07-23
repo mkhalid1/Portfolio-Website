f816404582333adb5e878f92b9bfbef4b2949ccc
Muhammad Junaid <mkhalid.bese16seecs@seecs.edu.pk>
2019-07-23 16:16:56 +0500

    Website Completed

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+
+def allblogs(Request):
+
+	blogs = Blog.objects
+	return render(Request, 'blog/blogs.html',{"blogs":blogs})
+
+def detail(Request, blog_id):
+
+	detailed_blog = get_object_or_404(Blog, pk= blog_id)
+	return render(Request, 'blog/detail.html', {"blog": detailed_blog})
